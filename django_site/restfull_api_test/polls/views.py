4c08604acab563ef1e9169221548d1b11839013e
DimKotyan <dimasmyslovv1234@gmail.com>
2022-01-27 17:02:37 +0300

    branch check

--- django_site/restfull_api_test/polls/views.py
+++ django_site/restfull_api_test/polls/views.py
@@ -3,6 +3,6 @@ from .models import Data
 
 
 def index(request):
-    latest_twenty = Data.objects.all()[:20]
+    latest_twenty = Data.objects.all()[1500:1550]
     return render(request, "polls/index.html", {'data' : latest_twenty})
 
