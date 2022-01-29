20a27d1b5f461b6796eacd185e0c9decfac4df5c
DimKotyan <dimasmyslovv1234@gmail.com>
2022-01-30 00:02:01 +0300

    auth v.0.99.4
    added changing tables on info change

--- django_site/restfull_api_test/polls/static/js/ajaxCall.js
+++ django_site/restfull_api_test/polls/static/js/ajaxCall.js
@@ -1,8 +1,4 @@
-function deleteCh()
-{
-    myChart.destroy()
-    myChart2.destroy()
-}
+
 
 function createChart1(label, dataset, dataset2){
     // var ctx = document.getElementById('myChart').getContext('2d');
@@ -65,8 +61,39 @@ $(document).ready(function(){
                     var air_temp = response['air_temp_avg']
                     var soil_temp = response['soil_temp_avg']
                     var humidity = response['relative_humidity_avg']
-                    var soil_moisture = response['soil_moisture']
+                    var soil_moisture = response['soil_moisture']                    
+                    
+                    tableParent = document.getElementById('myTable').parentNode
+                    $("#myTable").remove()
+                    tableChild = document.createElement('table')
+                    tableChild.id = 'myTable'
+                    tableChild.classList.add('table')
+                    var newRow = tableChild.insertRow(0);
+                    newRow.insertCell(0).outerHTML = '<th onclick="sortTable(0)">Дата </th>';
+                    newRow.insertCell(1).outerHTML = '<th onclick="sortTable(1)">Средняя температура воздуха, °C</th>';
+                    newRow.insertCell(2).outerHTML = '<th onclick="sortTable(2)">Относительная влажность воздуха, %</th>';
+                    newRow.insertCell(3).outerHTML = '<th onclick="sortTable(3)">Средняя температура почвы, °C</th>';
+                    newRow.insertCell(4).outerHTML = '<th onclick="sortTable(4)">Влажность почвы, %</th>';
+                    tableChild.appendChild(newRow);
+                    for (var i in dates)
+                    {
+                        var newRow = tableChild.insertRow(i.indexOf);
+                        newRow.insertCell(0).innerHTML = dates[i];
+                        newRow.insertCell(1).innerHTML = air_temp[i];
+                        newRow.insertCell(2).innerHTML = soil_temp[i];
+                        newRow.insertCell(3).innerHTML = humidity[i];
+                        newRow.insertCell(4).innerHTML = soil_moisture[i];            
+                        tableChild.appendChild(newRow);           
+                    }
 
+                    tableParent.appendChild(tableChild)
+
+                    $("#myTable tbody").remove()
+                    $('#myTable tr:has(td)').wrapAll('<tbody></tbody>')
+                    $("#myTable tbody").prependTo("#myTable")
+                    $('#myTable tr:has(th)').wrapAll('<thead></thead>')
+                    $("#myTable thead").prependTo("#myTable")
+                    
                     var S_0020CF3B_dataset_temp = 
                     [
                         {                     
@@ -110,24 +137,58 @@ $(document).ready(function(){
                 if (response.name == "Station_002099C5")
                 {                    
                     var dates = response['dates']
-                    var air_temp1 = response['air_temp_avg']
-                    var humidity1 = response['relative_humidity_avg']
+                    var air_temp = response['air_temp_avg']
+                    var humidity = response['relative_humidity_avg']
                     var soil_temp_1 = response['soil_temp_1']
                     var soil_temp_2 = response['soil_temp_2']
                     var soil_temp_3 = response['soil_temp_3']
+
+                    tableParent = document.getElementById('myTable').parentNode
+                    $("#myTable").remove()
+                    tableChild = document.createElement('table')
+                    tableChild.id = 'myTable'
+                    tableChild.classList.add('table')
+                    var newRow = tableChild.insertRow(0);
+                    newRow.insertCell(0).outerHTML = '<th onclick="sortTable(0)">Дата </th>';
+                    newRow.insertCell(1).outerHTML = '<th onclick="sortTable(1)">Средняя температура воздуха, °C</th>';
+                    newRow.insertCell(2).outerHTML = '<th onclick="sortTable(2)">Относительная влажность воздуха, %</th>';
+                    newRow.insertCell(3).outerHTML = '<th onclick="sortTable(3)">Средняя температура почвы 1, °C</th>';
+                    newRow.insertCell(4).outerHTML = '<th onclick="sortTable(4)">Средняя температура почвы 2, °C</th>';
+                    newRow.insertCell(5).outerHTML = '<th onclick="sortTable(5)">Средняя температура почвы 3, °C</th>';
+                    tableChild.appendChild(newRow);
+                    for (var i in dates)
+                    {
+                        var newRow = tableChild.insertRow(i.indexOf);
+                        newRow.insertCell(0).innerHTML = dates[i];
+                        newRow.insertCell(1).innerHTML = air_temp[i];
+                        newRow.insertCell(2).innerHTML = humidity[i];
+                        newRow.insertCell(3).innerHTML = soil_temp_1[i];
+                        newRow.insertCell(4).innerHTML = soil_temp_2[i];    
+                        newRow.insertCell(5).innerHTML = soil_temp_3[i];            
+                        tableChild.appendChild(newRow);           
+                    }
+
+                    tableParent.appendChild(tableChild)
+                    
+                    $("#myTable tbody").remove()
+                    $('#myTable tr:has(td)').wrapAll('<tbody></tbody>')
+                    $("#myTable tbody").prependTo("#myTable")
+                    $('#myTable tr:has(th)').wrapAll('<thead></thead>')
+                    $("#myTable thead").prependTo("#myTable")
+                    
 
                     var S_002099C5_dataset = 
                     [
                         {                     
                             label: 'Средняя температура воздуха, °C',
-                            data: air_temp1,
+                            data: air_temp,
                             backgroundColor: ['rgba(255, 255,255, 0.2)'],
                             borderColor: ['rgba(200, 0, 15, 1)'],
                             borderWidth: 2
                         },
                         {
                             label: 'Относительная влажность воздуха, %',
-                            data: humidity1,
+                            data: humidity,
                             backgroundColor: ['rgba(255, 255, 255, 0.2)'],
                             borderColor: ['rgba(9, 132, 0, 1)'],                            
                             borderWidth: 2
@@ -380,27 +441,27 @@ $(document).ready(function(){
 		// 			  }				
 		// 		  });
 				  
-		// 		  //сортировка таблицы	
-		// 		  document.addEventListener('DOMContentLoaded', () => {
-		// 		  const getSort = ({ target }) => {
-		// 		  const order = (target.dataset.order = -(target.dataset.order || -1));
-		// 		  const index = [...target.parentNode.cells].indexOf(target);
-		// 		  const collator = new Intl.Collator(['en', 'ru'], { numeric: true });
-		// 		  const comparator = (index, order) => (a, b) => order * collator.compare(
-		// 			  a.children[index].innerHTML,
-		// 			  b.children[index].innerHTML
-		// 		  );
+				  //сортировка таблицы	
+				  document.addEventListener('DOMContentLoaded', () => {
+				  const getSort = ({ target }) => {
+				  const order = (target.dataset.order = -(target.dataset.order || -1));
+				  const index = [...target.parentNode.cells].indexOf(target);
+				  const collator = new Intl.Collator(['en', 'ru'], { numeric: true });
+				  const comparator = (index, order) => (a, b) => order * collator.compare(
+					  a.children[index].innerHTML,
+					  b.children[index].innerHTML
+				  );
 
-		// 		  for(const tBody of target.closest('table').tBodies)
-		// 			  tBody.append(...[...tBody.rows].sort(comparator(index, order)));
+				  for(const tBody of target.closest('table').tBodies)
+					  tBody.append(...[...tBody.rows].sort(comparator(index, order)));
 
-		// 		  for(const cell of target.parentNode.cells)
-		// 			  cell.classList.toggle('sorted', cell === target);
-		// 	  };
+				  for(const cell of target.parentNode.cells)
+					  cell.classList.toggle('sorted', cell === target);
+			  };
 
-		// 	  document.querySelectorAll('.table thead').forEach(tableTH => tableTH.addEventListener('click', () => getSort(event)));
+			  document.querySelectorAll('.table thead').forEach(tableTH => tableTH.addEventListener('click', () => getSort(event)));
 
-		//   });
+		  });
 			  
                 
             }
