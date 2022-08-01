70ede35f1be8a2fcc03b3988b66b21686c1df0bf
DimKotyan <dimasmyslovv1234@gmail.com>
2022-08-01 19:15:40 +0300

    Migrated to MySQL database
    For more info redirect to DimKotyan

--- django_site/restfull_api_test/restfull_api_test/settings.py
+++ django_site/restfull_api_test/restfull_api_test/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'restfull_api_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'restfull_api_test',
+        'USER': 'django',
+        'PASSWORD': 'projectpilot',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'DEFAULT-CHARACTER-SET': 'utf-8'
     }
 }
 
