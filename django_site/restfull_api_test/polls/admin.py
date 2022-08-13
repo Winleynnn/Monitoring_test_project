77b0e9846bc64dedd51bb9488bc3631733a47324
DimKotyan <dimasmyslovv1234@gmail.com>
2022-08-13 03:42:19 +0300

    Made two new stations (from csv)
    If working incorrect, load data from datadump2.json

--- django_site/restfull_api_test/polls/admin.py
+++ django_site/restfull_api_test/polls/admin.py
@@ -12,3 +12,13 @@ class Station_002099C5_Admin(admin.ModelAdmin):
 @admin.register(Station_00000235)
 class Station_00000235_Admin(admin.ModelAdmin):
     list_display = ('id','date', 'air_temp_avg', 'relative_humidity_avg', 'dew_point', 'wind_speed_avg', 'wind_speed_max')
+
+@admin.register(Station_Jena)
+class Station_Jena_Admin(admin.ModelAdmin):
+    #date p T Tpot Tdew rh VPmax VPact VPdef sh H2OC rho wv max_wv wd
+    list_display = ('id','date', 'T', 'Tpot', 'Tdew', 'rh', 'VPmax', 'VPact', 'VPdef', 'sh', 'H2OC', 'rho', 'wv', 'max_wv', 'wd')
+
+@admin.register(Station_Gallipoli)
+class Station_Gallipoli_Admin(admin.ModelAdmin):
+     # date temp sunshine short_rad relative_humidity mean_sea_pressure soil_temp soil_moisture wind_speed wind_direction
+    list_display = ('id','date', 'sunshine', 'short_rad', 'relative_humidity', 'mean_sea_pressure', 'soil_temp', 'soil_moisture', 'wind_speed', 'wind_direction')
--- django_site/restfull_api_test/polls/models.py
+++ django_site/restfull_api_test/polls/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import csv
 
 # Create your models here.
 class Station_0020CF3B(models.Model):
@@ -36,6 +36,62 @@ class Station_00000235(models.Model):
 
     def __str__(self):
         return self.date
+
+class Station_Gallipoli(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.TextField(null=True)
+    temp = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    sunshine = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    short_rad = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    relative_humidity = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    mean_sea_pressure = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    soil_temp = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    soil_moisture = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    wind_speed = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    wind_direction = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return self.date
+
+class Station_Jena(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.TextField(null=True)
+    p = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    T = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    Tpot = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    Tdew = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    rh = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    VPmax = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    VPact = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    VPdef = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    sh = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    H2OC = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    rho = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    wv = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    max_wv = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    wd = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return self.date
+        
+# def jena_gallipoli():
+#     with open('jena_climate_2009_2016.csv', encoding='utf-8') as jena_file:
+#         reader = csv.reader(jena_file, delimiter=',')
+#         next(reader, None)
+#         for row in reader:
+#             #date p T Tpot Tdew rh VPmax VPact VPdef sh H2OC rho wv max_wv wd
+#              Station_Jena.objects.create(date=row[0], p=row[1], T=row[2], Tpot=row[3], 
+#              Tdew=row[4], rh=row[5], VPmax=row[6], VPact=row[7], VPdef=row[8], sh=row[9], 
+#              H2OC=row[10], rho=row[11], wv=row[12], max_wv=row[13], wd=row[14])
+#     with open('Hourly Weather Data in Gallipoli (2008-2021).csv', encoding='utf-8') as gal_file:
+#         reader = csv.reader(gal_file, delimiter=';')
+#         next(reader, None)
+#         for row in reader:
+#             # date temp sunshine short_rad relative_humidity mean_sea_pressure soil_temp soil_moisture wind_speed wind_direction
+#              Station_Gallipoli.objects.create(date=row[0], sunshine=row[1], short_rad=row[2], relative_humidity=row[3], 
+#              mean_sea_pressure=row[4], soil_temp=row[5], soil_moisture=row[6], wind_speed=row[7], wind_direction=row[8])
+
+# jena_gallipoli()
 
 def get_station_info():
     # login/pass: demo_api/demo4
@@ -171,10 +227,10 @@ def get_station_info():
     rel_hum = humidity['values']['avg']
     dew_p = dew_point['values']['avg']
     wind_avg = wind_speed_avg['values']['avg']
-    wind_max = wind_speed_max['values']['max']
+    row = wind_speed_max['values']['max']
 
     for i in range(1, len(air_t['values']['avg'])):
-        if ((air_temp[i] != None) & (rel_hum[i] != None) & (dew_p[i] != None) & (wind_avg[i] != None) & (wind_max[i] != None)):
-            Station_00000235.objects.create(date=dates[i], air_temp_avg=air_temp[i], relative_humidity_avg=rel_hum[i], dew_point=dew_p[i], wind_speed_avg=wind_avg[i], wind_speed_max=wind_max[i])
+        if ((air_temp[i] != None) & (rel_hum[i] != None) & (dew_p[i] != None) & (wind_avg[i] != None) & (row[i] != None)):
+            Station_00000235.objects.create(date=dates[i], air_temp_avg=air_temp[i], relative_humidity_avg=rel_hum[i], dew_point=dew_p[i], wind_speed_avg=wind_avg[i], wind_speed_max=row[i])
 
 # get_station_info()
