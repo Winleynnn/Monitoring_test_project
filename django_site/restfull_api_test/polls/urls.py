f64bdd0f7e7cb9d9f5488c360d69cd12345d2a29
DimKotyan <dimasmyslovv1234@gmail.com>
2023-11-12 13:40:59 +0500

    wtf is going on
    image starting to code after half a year break and be greeted with branch conflict AFTER you merged all of branches into one

--- django_site/restfull_api_test/polls/urls.py
+++ django_site/restfull_api_test/polls/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name = "index"),
     path('logout', views.logout_request, name = "Войти"),
     path('login', views.login_request, name = "Выйти"),
+    path('register', views.register, name = "Регистрация"),
+    path('feedbackform.html', views.feedback, name="Обратная связь")
 ]
